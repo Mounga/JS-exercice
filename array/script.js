a731331da9e69f0415804394457c971931d8b829
mounga <christopher.fourgeaud@gmail.com>
2017-07-05 11:41:23 +0200

    tableau de l'exercice 1 terminé.

--- array/script.js
+++ array/script.js
@@ -1,4 +1,10 @@
+//1er mini exercice
+//recuperer le 3eme element du tableau array;
+//recuperer l'element d'index 3 du tableau array;
 var array = ['Pomme','Cerise','Tomate','Citron', ['Orange','Poivron']];
 
 array[2];
 array[3]; 
+
+//1er exercice
+var array = ["janvier","février","mars","avril","mai","juin","juillet","aout","septembre","octobre","novembre","décembre"]
