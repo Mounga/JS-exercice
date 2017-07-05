9099fa0e1f562a8ebc2348822767a4ef0fb3211a
mounga <christopher.fourgeaud@gmail.com>
2017-07-05 11:45:43 +0200

    2eme exercice terminé.

--- array/script.js
+++ array/script.js
@@ -7,4 +7,9 @@ array[2];
 array[3]; 
 
 //1er exercice
-var array = ["janvier","février","mars","avril","mai","juin","juillet","aout","septembre","octobre","novembre","décembre"]
+var mois = ["janvier","février","mars","avril","mai","juin","juillet","aout","septembre","octobre","novembre","décembre"];
+
+//2éme exercice
+mois[2];
+
+//3éme exercice
